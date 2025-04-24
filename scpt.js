bf0f7fa1f2f40cbbc08ea622ffa0f33e7768456c
Ricky <twonine293@gmail.com>
2025-04-24 17:08:55 -0300

    Initial commit

---
+++ scpt.js
@@ -0,0 +1,8 @@
+
+  const menu = document.querySelector('.menu-interativo');
+  const botaoMenu = document.querySelector('.mn3');
+
+  botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('open');
+  });
+
